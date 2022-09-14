4ca9818fcef9166af58f703e7168d3686dcce610
adityahimaone <adityahimaone@gmail.com>
2022-09-14 16:45:18 +0700

    config: install eslint

--- screens/AllExpenses.jsx
+++ screens/AllExpenses.jsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-const AllExpenses = () => {
+function AllExpenses() {
   return (
     <View>
       <Text>AllExpenses</Text>
     </View>
   );
-};
+}
 
 export default AllExpenses;
 
